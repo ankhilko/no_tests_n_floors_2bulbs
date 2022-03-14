1411a9e2597feaf915f728939d4d952b55eba5a8
ankhilko <fieldstreet.27@gmail.com>
2022-03-14 21:06:56 +0300

    first version added

---
+++ main.py
@@ -0,0 +1,10 @@
+def calc2bulbs(n_floors):
+    """
+    return a minimal number of needed tests
+    """
+    return min(i + int(n_foorls / i - 1) for i in range(1, n_floors + 1))
+
+n = input("Enter the numbers of floors: ")
+x = calc2bulbs(n)
+print(f"{x} attempt are needed minimal")
+
