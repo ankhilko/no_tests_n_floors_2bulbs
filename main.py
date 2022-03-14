50e1339daada99e63674ffd0a8118b02bb98826a
ankhilko <fieldstreet.27@gmail.com>
2022-03-14 21:15:35 +0300

    name of the 'variable' corrected

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def calc2bulbs(n_floors):
     """
     return a minimal number of needed tests
     """
-    return min(i + int(n_foorls / i - 1) for i in range(1, n_floors + 1))
+    return min(i + int(n_floors / i - 1) for i in range(1, n_floors + 1))
 
 n = int(input("Enter the numbers of floors: "))
 x = calc2bulbs(n)
