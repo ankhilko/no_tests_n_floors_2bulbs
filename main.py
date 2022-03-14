e63da29a9a1f737fb9a2135d1f1c7ee677db652a
ankhilko <fieldstreet.27@gmail.com>
2022-03-14 21:11:44 +0300

    corrected: input converted to int

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def calc2bulbs(n_floors):
     """
     return min(i + int(n_foorls / i - 1) for i in range(1, n_floors + 1))
 
-n = input("Enter the numbers of floors: ")
+n = int(input("Enter the numbers of floors: "))
 x = calc2bulbs(n)
 print(f"{x} attempt are needed minimal")
 
